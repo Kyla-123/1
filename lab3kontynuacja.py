65d76e1e35c7cc5f4ff2c107f871a25fd467827a
Kyla-123 <kyla4321@gmail.com>
2022-11-06 13:30:47 +0100

    cokolwiek

---
+++ lab3kontynuacja.py
@@ -0,0 +1,28 @@
+
+
+#for (i) in range(0,101):
+#    print(i, end=' ')
+#print()
+#for (j) in range(100,-1,-1):
+#    print(j, end=' ')
+#print()
+#for (k) in range(7,84,7):
+#    print(k, end=' ')
+#print()
+#for (l) in range(20,-2, -2):
+#    print(l, end=' ')
+#4
+#print()
+#m=int(input('podaj liczbę gwiazdek w linii:'))
+#for(m) in range(m):
+#    print('*'*m)
+
+#print()
+#n=int(input('podaj liczbę gwiazdek w linii:'))
+#for(n) in range(n):
+#    print('*', end='')
+x=int(input('podaj liczbę całkowitą:'))
+for (z) in range(x):
+    for (y) in range(x):
+        print('*',end=' ')
+    print(' ')
