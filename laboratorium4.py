a1e6258937c091401950453eb3fd88a0fd8fa1fd
Kyla-123 <kyla4321@gmail.com>
2022-11-06 14:23:00 +0100

    cokolwiek23

--- laboratorium4.py
+++ laboratorium4.py
@@ -4,30 +4,40 @@
 #lista_zakupów[2]='mleko'
 #print(lista_zakupów)
 #print(lista_zakupów[-2]+","+lista_zakupów[-1])
-import random
-x=int(input('podaj liczbę elementów'))
-lista1=[]
-for i in range(x):
-    n=random.randint(1,10)
-    lista1.append(n)
-print(lista1)
+#import random
+#x=int(input('podaj liczbę elementów'))
+#lista1=[]
+#for i in range(x):
+#    n=random.randint(1,10)
+#    lista1.append(n)
+#print(lista1)
 
-x1=int(input('podaj liczbę elementów dla drugiej listy'))
-lista2=[]
-for i in range(x):
-    n=random.randint(5,15)
-    lista2.append(n)
-print(lista2)
+#x1=int(input('podaj liczbę elementów dla drugiej listy'))
+#lista2=[]
+#for i in range(x):
+#    n=random.randint(5,15)
+#    lista2.append(n)
+#print(lista2)
+#
+#x2=int(input('podaj liczbę której szukasz'))
+#if x2 in lista1:
+#    print('liczba występuje w: lista1')
+#elif x2 in lista2:
+#    print('liczba występuje w: lista2')
+#else:
+#    print("nie takiej liczby w obu zestawach")
 
-x2=int(input('podaj liczbę której szukasz'))
-if x2 in lista1:
-    print('liczba występuje w: lista1')
-elif x2 in lista2:
-    print('liczba występuje w: lista2')
-else:
-    print("nie takiej liczby w obu zestawach")
+#lista_1_2=lista1+lista2
+#lista_1_2.sort()
+#print(lista_1_2)
 
-lista_1_2=lista1+lista2
-lista_1_2.sort()
-print(lista_1_2)
+zwierzęta=[]
+for i in range(4):
+    x=input('podaj zwierzę:')
+    zwierzęta.append(x)
+print(zwierzęta)
 
+zwierzęta.sort()
+print(zwierzęta)
+
+print(zwierzęta[0] ,zwierzęta[-3:])
